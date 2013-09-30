144f86807d0be476e01f6752d4544ddb4cd2c2e2
zunlin1234 <zunlin1234@gmail.com>
2013-09-30 02:57:21 -0700

    xxx

--- p2p_client.py
+++ p2p_client.py
@@ -457,15 +457,15 @@ class GuiClient(NodeService,QtGui.QMainWindow):
 	def updateList(self):
 		mylogger.info("[updateList]...")
 		# update local and remote files
-		print "local_update",NodeService.is_local_updated()
-		print "remote_update",NodeService.is_remote_updated()
+		print "local_update",NodeService.is_local_updated(self)
+		print "remote_update",NodeService.is_remote_updated(self)
 		if not NodeService.is_local_updated(self):
 			NodeService.update_local_list(self)
-			print "after,local_update",NodeService.is_local_updated()
+			print "after,local_update",NodeService.is_local_updated(self)
 			self.setLocal()
 		if not NodeService.is_remote_updated(self):
 			NodeService.update_remote_list(self)
-			print "after,remote_update",NodeService.is_remote_updated()
+			print "after,remote_update",NodeService.is_remote_updated(self)
 			self.setRemote()
 		mylogger.info("[updateList] finished")
 
